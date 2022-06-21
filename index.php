fcd477da53b4de2a8e2eaba3aa6caa20d74a61bb
vikas-saini07 <56854067+vikas-saini07@users.noreply.github.com>
2022-06-21 15:00:01 +1000

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <ul>
             <li><a href="./directapi/nvp/welcome.php">Direct API NVP methos using authentication API</a></li>
             <li><a href="./hostedchkout/newhostchk/welcome.html">MPGS Hosted checkout- New Method</a></li>
-            <li><a href="./hostedchkout/oldhosted/welcome.html">MPGS Hosted checkout- Old Method</a></li>
+            <li><a href="./hostedchkout/oldhosted/welcome.php">MPGS Hosted checkout- Old Method</a></li>
         </ul>
     </body>
 </html>
